ac4fab8273efea1feb3221f52758ebca4dadb91e
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-11-20 23:57:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from testers import Tester
+from Manual import Manual
 if __name__ == '__main__':
   tester = Tester()
   tester.test_number_to_pair(4, 'White', 'Brown')
@@ -6,4 +7,6 @@ if __name__ == '__main__':
   tester.test_pair_to_number('Black', 'Orange', 12)
   tester.test_pair_to_number('Violet', 'Slate', 25)
   tester.test_pair_to_number('Red', 'Orange', 7)
+  print("Reference Manual: ")
+  Manual().reference_manual()
   print('Done :)')
