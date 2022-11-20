1fd52dca7b46935273751124167df6b82961db4b
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-11-20 23:57:09 +0530

    Update Manual.py

--- Manual.py
+++ Manual.py
@@ -6,9 +6,9 @@ class Manual:
         self.MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
     def color_pair_to_string(self,major_color, minor_color):
         return f'{major_color} {minor_color}'
-    def color_mapping(self,major_color, minor_color):
-        color_names = self.color_pair_to_string(major_color, minor_color)
-        color_number = pairNumberExtractor.get_pair_number_from_color(major_color, minor_color)
-        return f'{color_names} {color_number}'
-
-
+    def reference_manual(self):
+        for major_color in self.MAJOR_COLORS:
+            for minor_color in self.MINOR_COLORS:
+                color_names = self.color_pair_to_string(major_color, minor_color)
+                color_number = pairNumberExtractor.get_pair_number_from_color(major_color, minor_color)
+                print(f'{color_names} {color_number}')
